ab9f99b52d00c1988fa79c76bdc0793ef7062e2c
Alexsannd <alexsandersannestedt@gmail.com>
2022-09-05 20:50:16 +0200

    Code cleanup and improvements. Added footer

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 let courses = 24;
-var examReady = [false, false, false, false, false, false];
-var examReadyID = [0, 1, 2, 8, 9, 15];
+let examReady = [false, false, false, false, false, false];
+let examReadyID = [0, 1, 2, 8, 9, 15];
+//Times to multiply by
+let timesPoints = 2;
+
 function myFunction(clicked_id) {
     document.getElementById("myDropdown" + clicked_id.substring(8)).classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
 
 
     if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            const openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
-
             }
         }
     }
 }
+
+//Executes when you press a grade in a dropdown
 function selectGrade(clicked_id) {
     let number;
+    timesPoints=2;
     switch (clicked_id.charAt(clicked_id.length-1)) {
         //Gymnasiearbete
         case "G": {
@@ -39,101 +43,55 @@ function selectGrade(clicked_id) {
                     break;
             }
         } break;
-        //Long aka 150 points
-        case "L": {
-            number = Number(clicked_id.slice(0, -2));
-            switch (clicked_id.charAt(clicked_id.length-2)) {
-                case "A":
-                    document.getElementById("gradeBtn" + number).textContent = "A";
-                    document.getElementById("gradeValue" + number).textContent = "3000";
-                    document.getElementById("numberValue" + number).textContent = "20";
-                    break;
-                case "B":
-                    document.getElementById("gradeBtn" + number).textContent = "B";
-                    document.getElementById("gradeValue" + number).textContent = "2625";
-                    document.getElementById("numberValue" + number).textContent = "17.5";
-                    break;
-                case "C":
-                    document.getElementById("gradeBtn" + number).textContent = "C";
-                    document.getElementById("gradeValue" + number).textContent = "2250";
-                    document.getElementById("numberValue" + number).textContent = "15";
-                    break;
-                case "D":
-                    document.getElementById("gradeBtn" + number).textContent = "D";
-                    document.getElementById("gradeValue" + number).textContent = "1875";
-                    document.getElementById("numberValue" + number).textContent = "12.5";
-                    break;
-                case "E":
-                    document.getElementById("gradeBtn" + number).textContent = "E";
-                    document.getElementById("gradeValue" + number).textContent = "1500";
-                    document.getElementById("numberValue" + number).textContent = "10";
-                    break;
-            }
-        } break;
+        //Long aka 150 points.
+        case "L": timesPoints=3; break;
+
         //Short aka 50 points
-        case "S": {
-            number = Number(clicked_id.slice(0, -2));
-            switch (clicked_id.charAt(clicked_id.length-2)) {
-                case "A":
-                    document.getElementById("gradeBtn" + number).textContent = "A";
-                    document.getElementById("gradeValue" + number).textContent = "1000";
-                    document.getElementById("numberValue" + number).textContent = "20";
-                    break;
-                case "B":
-                    document.getElementById("gradeBtn" + number).textContent = "B";
-                    document.getElementById("gradeValue" + number).textContent = "875";
-                    document.getElementById("numberValue" + number).textContent = "17.5";
-                    break;
-                case "C":
-                    document.getElementById("gradeBtn" + number).textContent = "C";
-                    document.getElementById("gradeValue" + number).textContent = "750";
-                    document.getElementById("numberValue" + number).textContent = "15";
-                    break;
-                case "D":
-                    document.getElementById("gradeBtn" + number).textContent = "D";
-                    document.getElementById("gradeValue" + number).textContent = "625";
-                    document.getElementById("numberValue" + number).textContent = "12.5";
-                    break;
-                case "E":
-                    document.getElementById("gradeBtn" + number).textContent = "E";
-                    document.getElementById("gradeValue" + number).textContent = "500";
-                    document.getElementById("numberValue" + number).textContent = "10";
-                    break;
-            }
-        } break;
+        case "S": timesPoints=1; break;
     }
 
     number = Number(clicked_id.slice(0, -1));
     switch (clicked_id.charAt(clicked_id.length-1)) {
-        case "A": document.getElementById("gradeBtn" + number).textContent = "A";
-            document.getElementById("gradeValue" + number).textContent = "2000";
-            document.getElementById("numberValue" + number).textContent = "20"; break;
-        case "B": document.getElementById("gradeBtn" + number).textContent = "B";
-            document.getElementById("gradeValue" + number).textContent = "1750";
-            document.getElementById("numberValue" + number).textContent = "17.5"; break;
-        case "C": document.getElementById("gradeBtn" + number).textContent = "C";
-            document.getElementById("gradeValue" + number).textContent = "1500";
-            document.getElementById("numberValue" + number).textContent = "15"; break;
-        case "D": document.getElementById("gradeBtn" + number).textContent = "D";
-            document.getElementById("gradeValue" + number).textContent = "1250";
-            document.getElementById("numberValue" + number).textContent = "12.5"; break;
-        case "E": document.getElementById("gradeBtn" + number).textContent = "E";
-            document.getElementById("gradeValue" + number).textContent = "1000";
-            document.getElementById("numberValue" + number).textContent = "10"; break;
-        case "F": document.getElementById("gradeBtn" + number).textContent = "F";
+        case "A":
+            document.getElementById("gradeBtn" + number).textContent = "A";
+            document.getElementById("gradeValue" + number).textContent = 1000 * timesPoints;
+            document.getElementById("numberValue" + number).textContent = "20";
+            break;
+        case "B":
+            document.getElementById("gradeBtn" + number).textContent = "B";
+            document.getElementById("gradeValue" + number).textContent = 875 * timesPoints;
+            document.getElementById("numberValue" + number).textContent = "17.5";
+            break;
+        case "C":
+            document.getElementById("gradeBtn" + number).textContent = "C";
+            document.getElementById("gradeValue" + number).textContent = 750 * timesPoints;
+            document.getElementById("numberValue" + number).textContent = "15";
+            break;
+        case "D":
+            document.getElementById("gradeBtn" + number).textContent = "D";
+            document.getElementById("gradeValue" + number).textContent = 625 * timesPoints;
+            document.getElementById("numberValue" + number).textContent = "12.5";
+            break;
+        case "E":
+            document.getElementById("gradeBtn" + number).textContent = "E";
+            document.getElementById("gradeValue" + number).textContent = 500 * timesPoints;
+            document.getElementById("numberValue" + number).textContent = "10";
+            break;
+        case "F":
+            document.getElementById("gradeBtn" + number).textContent = "F";
             document.getElementById("gradeValue" + number).textContent = "0";
-            document.getElementById("numberValue" + number).textContent = "0"; break;
-        case "-": document.getElementById("gradeBtn" + number).textContent = "-";
+            document.getElementById("numberValue" + number).textContent = "0";
+            break;
+        case "-":
+            document.getElementById("gradeBtn" + number).textContent = "-";
             document.getElementById("gradeValue" + number).textContent = "-";
-            document.getElementById("numberValue" + number).textContent = "-"; break;
+            document.getElementById("numberValue" + number).textContent = "-";
+            break;
     }
-    let done = 0;
-    let totalNumberValue = 0;
+
     let totalGradeValue = 0;
     for (let i = 0; i < courses; i++) {
         if (!(document.getElementById("numberValue" + i).textContent == "-" || document.getElementById("numberValue" + i).textContent == "")) {
-            done++;
-            totalNumberValue += Number(document.getElementById("numberValue" + i).textContent);
             totalGradeValue += Number(document.getElementById("gradeValue" + i).textContent)
 
         }
@@ -143,10 +101,8 @@ function selectGrade(clicked_id) {
         if (document.getElementById("numberValue" + examReadyID[i]).textContent == "-" ||
             document.getElementById("numberValue" + examReadyID[i]).textContent == "0")
             examReady[i] = false; else examReady[i] = true;
-            if (examReady[i] == false) document.getElementById("lastExtra").textContent = "Du kan inte skriva examen";
+        if (examReady[i] == false) document.getElementById("lastExtra").textContent = "Du kan inte skriva examen";
     }
 
-    document.getElementById("numberValue" + Number(courses+1)).textContent = Number(totalNumberValue/done);
-    document.getElementById("gradeValue" + Number(courses+1)).textContent = totalGradeValue;
-
+    document.getElementById("gradeValue" + Number(courses+1)).textContent = (totalGradeValue/2400).toFixed(2);
 }
